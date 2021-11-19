Fixed bug in storage of data for gradient methods. Changed code so that only 1 regularization value is used for each test and not the individual optimal value.

find_optimal_hyperparams_recalc.py
@@ -110,6 +110,7 @@ def main(argv):
         for i, (rho, sigma, leakage) in enumerate(zip(rhos_mat, sigmas_mat, leakages_mat)):
             folder = '%s_more_noisetest_rho%0.1f_sigma%1.1e_leakage%0.3f_win_%s_bias_%s_tau%0.2f_%dnodes_%dtrain_%dreals_noisetype_%s_traintype_%s/' % (system, rho,    sigma, leakage, win_type, bias_type, tau, res_size, train_time, noise_realizations, noisetype, traintype)
             folder_new = '%s_more_noisetest_rho%0.1f_sigma%1.1e_leakage%0.3f_win_%s_bias_%s_tau%0.2f_%dnodes_%dtrain_%dreals_noisetype_%s_traintype_%s/' % (system, rho,sigma, leakage, win_type, bias_type, tau,res_size, train_time, noise_realizations, noisetype, traintype)
+
             filename_sf = 'stable_frac_%dnodes_%dtrainiters_%dnoisereals_raytest.csv' % (res_size, train_time, noise_realizations)
             filename_sf_reg = 'stable_frac_alpha_%dnodes_%dtrainiters_%dnoisereals_raytest.csv' % (res_size, train_time, noise_realizations)
             noisevals_loop = noisevals
@@ -151,7 +152,7 @@ def main(argv):
                 best_reg_vt, temp = mode(stable_frac_alpha[max_noise_itr_vt])
                 #print(count_elems(stable_frac_alpha[max_noise_itr]))
                 best_folder_vt  = folder_new
-    """
+
     print('Best Valid Time: %f' % best_valid_time)
     print('Best Spectral Radius: %f' % best_rho_vt)
     print('Best Input Weight: %f' % best_sigma_vt)
@@ -159,7 +160,6 @@ def main(argv):
     print('Best Noise Magnitude: %e' % best_noise_vt)
     print('Best Regularization: %e' % best_reg_vt)
     print(best_folder_vt[:-1])
-    """
 
     best_stable_frac_rho = np.zeros(rhos.size)
     best_sigma_rho       = np.zeros(rhos.size)

ks_etdrk4.py
@@ -93,18 +93,19 @@ def kursiv_forecast_pred(u, params):
 
 
 @jit(nopython = True, fastmath = True)
-def kursiv_predict(u0, tau = 0.25, N = 64, d = 22, T = 100, params = np.array([[],[]], dtype = np.complex128)):
+def kursiv_predict(u0, tau = 0.25, N = 64, d = 22, T = 100, params = np.array([[],[]], dtype = np.complex128), int_steps = 1):
     if params.size == 0:
         new_params = precompute_KS_params(N, d, tau)
     else:
         new_params = params
-    steps = T
+    steps = T*int_steps
 
-    u_arr = np.zeros((N, steps+1))
+
+    u_arr = np.zeros((N, steps+int_steps))
     u_arr[:,0] = u0
-    for i in range(steps):
+    for i in range(steps+int_steps-1):
         u_arr[:,i+1] = kursiv_forecast(u_arr[:,i], new_params)
-    return u_arr, new_params
+    return np.ascontiguousarray(u_arr[:,::int_steps]), new_params
 
 @jit(nopython = True, fastmath = True)
 def kursiv_predict_pred(u0_array, tau = 0.25, N = 64, d = 22, T = 100, params = np.array([[],[]], dtype = np.complex128)):

res_stability_test_driver_15.py
@@ -3,33 +3,36 @@ import numpy as np
 import time
 from itertools import product
 #from itertools import enumerate
-#rhos = np.array([0.1,0.3,0.5,0.7])
+#rhos = np.array([0.1, 0.3, 0.5, 0.7])
 rhos = np.array([0.1])
-#rhos = np.array([0.1,0.2])
-sigmas = np.array([1.0])
+#sigmas = np.array([0.5, 1.0, 1.5, 2.0])
+sigmas = np.array([0.5])
 #sigmas  = np.array([0.1,0.2,0.3,0.4])
 #leakages = np.array([0.5,0.625,0.75,0.875,1.0])
 #leakages = np.array([0.25, 0.5, 0.75, 1.0])
-leakages = np.array([0.5])
+leakages = np.array([0.6])
 rhos_mat, sigmas_mat, leakages_mat = np.meshgrid(rhos, sigmas, leakages)
 tau          = 0.25
 rhos_mat     = rhos_mat.flatten()
 sigmas_mat   = sigmas_mat.flatten()
 leakages_mat = leakages_mat.flatten()
 
-#noisetypes = ['none', 'gaussian']
-noisetypes = ['none']
-traintype = 'gradientk2'
-no        = 1
-res_sizes  = [1000]
-trainlen  = 9000
-win_types  = ['full']
+#noisetypes = ['none']
+noisetypes = ['gaussian']*2
+traintypes = ['normal']*2
+#traintypes = ['gradientk%d' % k for k in np.arange(1,11)]
+nos        = [1]*2
+res_sizes  = [175, 300]
+trainlens  = [4250, 2250]
+win_types  = ['full']*2
 system    = 'KS'
 
 bias_type = 'new_random'
 
-for noisetype, res_size, win_type in product(noisetypes, res_sizes, win_types):
+for noisetype, res_size, win_type, traintype, no, trainlen in zip(noisetypes, res_sizes, win_types, traintypes, nos, trainlens):
     for rho, sigma, leakage in zip(rhos_mat, sigmas_mat, leakages_mat):
-        testname = '%s_%s_%s_%d_rho%0.1f_sigma%0.1e_leakage%0.1f' % (system, traintype, noisetype, no, rho, sigma, leakage)
-        os.system('python slurm-launch.py --exp-name %s --command "python -u climate_replication_test.py --system=%s --noisetype=%s --traintype=%s -r %d --rho=%f --sigma=%f --leakage=%f --win_type=%s --bias_type=%s --tau=%f -N %d -T %d --res=25 --tests=10 --trains=25 --debug=False" --num-nodes 12 --load-env "conda activate  reservoir-rls" -t 2:00:00' % (testname, system, noisetype, traintype, no, rho, sigma, leakage, win_type, bias_type, tau, res_size, trainlen))
+        testname = '%s_%s_%s_%d_%dnodes_rho%0.1f_sigma%0.1e_leakage%0.1f' % (system, traintype, noisetype, res_size, no, rho, sigma, leakage)
+        os.system('python slurm-launch.py --exp-name %s --command "python -u climate_replication_test.py --system=%s --noisetype=%s --traintype=%s -r %d --rho=%f --sigma=%f --leakage=%f --win_type=%s --bias_type=%s --tau=%f -N %d -T %d --res=25 --tests=10 --trains=25 --debug=False" --num-nodes 8 --load-env "conda activate  reservoir-rls" -t 2:00:00 -A physics' % (testname, system, noisetype, traintype, no, rho, sigma, leakage, win_type, bias_type, tau, res_size, trainlen))
+        time.sleep(1)
+
 