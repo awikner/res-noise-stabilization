Fixed bug in calculation of valid times.

climate_replication_test.py
@@ -267,7 +267,7 @@ class RungeKutta:
         self.u_arr_test = u_arr[:, ttsplit:]
 
 
-#@jit(nopython=True, fastmath=True)
+@jit(nopython=True, fastmath=True)
 def RungeKuttawrapped(x0=2, y0=2, z0=23, h=0.01, tau=0.1, T=300, ttsplit=5000, u0=0, system='lorenz', params=np.array([[], []], dtype=np.complex128)):
     # Numba function for obtaining training and testing dynamical system time series data
     if system == 'lorenz':
@@ -294,7 +294,7 @@ def RungeKuttawrapped(x0=2, y0=2, z0=23, h=0.01, tau=0.1, T=300, ttsplit=5000, u
     return u_arr_train, u_arr_test, ttsplit, new_params
 
 
-#@jit(nopython=True, fastmath=True)
+@jit(nopython=True, fastmath=True)
 def RungeKuttawrapped_pred(h=0.01, tau=0.1, T=300, ttsplit=5000, u0_array=np.array([[], []], dtype=np.complex128), system='lorenz', params=np.array([[], []], dtype=np.complex128)):
     # Numba function for obtaining training and testing dynamical system time series data for a set of initial conditions.
     # This is used during test to compute the map error instead of a for loop over the entire prediction period.
@@ -330,7 +330,7 @@ def getX(res, rk, x0=1, y0=1, z0=1):
 
     return res.X
 
-#@jit(nopython=True, fastmath=True)
+@jit(nopython=True, fastmath=True)
 def getXwrapped(u_training, res_X, Win, W_data, W_indices, W_indptr, W_shape, leakage, noise, noisetype='none', noise_scaling=0, noise_realization=0, traintype='normal'):
     # Numba compatible function for obtaining reservoir states using various types of noise.
     # Generally returns an array of reservoir states and the noiseless training data used as input.
@@ -471,7 +471,7 @@ def getXwrapped(u_training, res_X, Win, W_data, W_indices, W_indptr, W_shape, le
 
 
 """
-#@jit(nopython = True, fastmath = True)
+@jit(nopython = True, fastmath = True)
 def getjacobian(Win, W, Wout, Dn):
     jacsize  = Wout.shape[1]-1
     res_size = Win.shape[0]
@@ -580,7 +580,7 @@ def get_states(res, rk, noise, noisetype='none', noise_scaling=0, noise_realizat
             skip, noise, noisetype, noise_scaling, noise_realizations, traintype)
 
 
-#@jit(nopython=True, fastmath=True)
+@jit(nopython=True, fastmath=True)
 def get_states_wrapped(u_arr_train, res_X, Win, W_data, W_indices, W_indptr, W_shape, leakage, skip, noise, noisetype='none',
         noise_scaling=0, noise_realizations=1, traintype='normal', q=0):
     # Numba compatible function to obtain the matrices used to train the reservoir using either linear regression or a sylvester equation.
@@ -1010,7 +1010,7 @@ def get_states_wrapped(u_arr_train, res_X, Win, W_data, W_indices, W_indptr, W_s
         return [data_trstates, states_trstates, Y_train, X_train, gradient_reg]
 
 
-#@jit(nopython=True, fastmath=True)
+@jit(nopython=True, fastmath=True)
 def getD(u_arr_train, res_X, Win, W_data, W_indices, W_indptr, W_shape, leakage, noise, skip, noisetype='none',
          noise_scaling=0, noise_realizations=1, traintype='normal'):
     n, d = u_arr_train.shape
@@ -1030,7 +1030,7 @@ def predict(res, u0,  steps=1000):
     return Y
 
 
-#@jit(nopython=True, fastmath=True)
+@jit(nopython=True, fastmath=True)
 def predictwrapped(res_X, Win, W_data, W_indices, W_indptr, W_shape, Wout, leakage, u0, steps):
     # Numba compatible prediction function
     Y = np.empty((Win.shape[1]-1, steps + 1))
@@ -1098,7 +1098,7 @@ def test(res, noise_in, rktest_u_arr_train_nonoise, rktest_u_arr_test, true_pmap
     return stable_count/num_tests, mean_rms, max_rms, variances, valid_time, rms, preds, wass_dist, pmap_max, pmap_max_wass_dist
 
 
-#@jit(nopython=True, fastmath=True)
+@jit(nopython=True, fastmath=True)
 def testwrapped(res_X, Win, W_data, W_indices, W_indptr, W_shape, Wout, leakage, rktest_u_arr_train_nonoise, rktest_u_arr_test,   true_pmap_max, num_tests, rkTime, split, noise_in, showMapError=True,   showTrajectories=True, showHist=True, system='lorenz', tau=0.1, params=np.array([[], []], dtype=np.complex128), pmap=False, max_valid_time = 500):
     # Numba compatable function for testing trained reservoir performance against true system time series
     stable_count = 0
@@ -1188,13 +1188,26 @@ def testwrapped(res_X, Win, W_data, W_indices, W_indptr, W_shape, Wout, leakage,
                 if error[j] < vt_cutoff and check_vt:
                     valid_time[i,k] = j
                 else:
+                    #if check_vt:
+                    """
+                    print(j)
+                    print('Prediction at VT overflow:')
+                    print(pred[:5,j])
+                    print('Truth at VT overflow:')
+                    print(rktest_u_arr_test[:5,k*max_valid_time+j,i])
+                    """
                     check_vt = False
             res_X = np.zeros((res_X.shape[0], max_valid_time+2))
             res_X, p = getXwrapped(np.ascontiguousarray(
                 rktest_u_arr_test[:, k*max_valid_time:(k+1)*max_valid_time+1, i]), res_X, Win, W_data, W_indices, W_indptr, W_shape, leakage, noise_in)
+            """
+            print('Res value after synchronization:')
+            print(res_X[:5,-2])
+            """
             if k < (num_vt_tests - 1):
                 pred = predictwrapped(res_X, Win, W_data, W_indices, W_indptr, W_shape,
                     Wout, leakage, u0=rktest_u_arr_test[:, (k+1)*max_valid_time, i], steps=max_valid_time-1)
+                check_vt = True
         if array_compute:
             if system == 'lorenz':
                 rkmap_u_arr_train = RungeKuttawrapped_pred(u0_array=np.stack((pred_full[0]*7.929788629895004,

helpers/start_deepthought2_run.py
@@ -1,5 +1,6 @@
+import numpy as np
 import os
-import subproccess
+import subprocess
 import re
 import time
 import sys
@@ -9,10 +10,10 @@ def start_deepthought2_run(system = 'KS', traintype = 'normal', noisetype = 'gau
         leakage = 1.0, tau = 0.25, win_type = 'full', bias_type = 'old',\
         noise_values_array = np.logspace(-3, 0, num = 19, base = 10)[5:11], \
         alpha_values = np.append(0., np.logspace(-7, -3, 9)), num_res = 3,\
-        num_trains = 4, num_tests = 4, metric = 'mss_var', returnall = False, \
-        savepred = False, savetime = False, max_valid_time = 500, debug = False, \
-        num_nodes = 4, cpus_per_node = 20, runtime = '2:00:00', account = 'physics-hi',\
-        debug_part = False, just_process = False):
+        num_trains = 4, num_tests = 4, metric = 'mss_var', machine = 'deepthought2', \
+        returnall = False, savepred = False, savetime = False, max_valid_time = 500, \
+        debug = False, num_nodes = 4, cpus_per_node = 20, runtime = '2:00:00', \
+        account = 'physics-hi',debug_part = False, just_process = False, parallel = True):
 
     noise_values_str = '%e' % noise_values_array[0]
     for noise in noise_values_array[1:]:
@@ -47,13 +48,18 @@ def start_deepthought2_run(system = 'KS', traintype = 'normal', noisetype = 'gau
     else:
         savetime_str = 'False'
 
+    if parallel:
+        parallel_str = 'True'
+    else:
+        parallel_str = 'False'
+
 
 
 
     testname = '%s_%s_%s_%d_%dnodes_%dtrain_rho%0.1f_sigma%0.1e_leakage%0.1f_tau%0.3f' % \
-            (system, traintype, noisetype, no, res_size, trainlen, rho, sigma, leakage,  tau)
-    options_str = '--savepred=%s --system=%s --noisetype=%s --traintype=%s -r %d --rho=%f --sigma=%f --leakage=%f --win_type=%s --bias_type=%s --tau=%f -N %d -T %d --res=%d --tests=%d --trains=%d --debug=%s --metric=%s --returnall=%s --savetime=%s --noisevals=%s --regvals=%s --maxvt=%d' % (savepred_str, system, noisetype, traintype, no,  rho,sigma, leakage, win_type, bias_type, tau, res_size, trainlen, num_res, num_tests, num_trains, debug_str, metric, returnall_str, savetime_str,
-            noise_values_str, reg_values_str, max_valid_time)
+            (system, traintype, noisetype, noise_realizations, res_size, trainlen, rho, sigma, leakage,  tau)
+    options_str = '--savepred=%s --system=%s --noisetype=%s --traintype=%s -r %d --rho=%f --sigma=%f --leakage=%f --win_type=%s --bias_type=%s --tau=%f -N %d -T %d --res=%d --tests=%d --trains=%d --debug=%s --metric=%s --returnall=%s --savetime=%s --noisevals=%s --regvals=%s --maxvt=%d --machine=%s --parallel=%s' % (savepred_str, system, noisetype, traintype, noise_realizations,  rho,sigma, leakage, win_type, bias_type, tau, res_size, trainlen, num_res, num_tests, num_trains, debug_str, metric, returnall_str,
+            savetime_str, noise_values_str, reg_values_str, max_valid_time, machine, parallel_str)
     input_str = 'python slurm-launch.py --exp-name %s --command "python -u climate_replication_test.py %s" --num-nodes %d --cpus-per-node=%d --load-env "conda activate reservoir-rls" -t %s -A %s %s' % (testname, options_str, num_nodes, cpus_per_node, runtime, account, debug_part_str)
     print(input_str)
     run_out = subprocess.check_output(input_str, shell=True)