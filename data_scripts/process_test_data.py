Added option to save gradient regularization eigenvalues. Fixed gradient regularization so that it works properly for 1 noise iteration.

data_scripts/process_test_data.py
@@ -36,7 +36,7 @@ def main(argv):
 
     tic = time.perf_counter()
 
-    root_folder, top_folder, run_name, system, noisetype, traintype, savepred, save_time_rms, squarenodes, rho,\
+    root_folder, top_folder, run_name, system, noisetype, traintype, savepred, save_time_rms, save_eigenvals, squarenodes, rho,\
         sigma, leakage, win_type, bias_type, tau, res_size, train_time, noise_realizations, noise_streams_per_test,\
         noise_values_array, alpha_values, res_per_test, num_trains, num_tests, debug_mode, pmap, metric, return_all,\
         ifray, machine, max_valid_time, prior, import_res, import_train, import_test, import_noise, reg_train_frac,\
@@ -108,6 +108,8 @@ def main(argv):
         rms = np.zeros((res_per_test, train_vals.size, num_tests, noise_vals.size, (rkTime-split), alpha_values.size, reg_train_times.size))
     if pmap:
         pmap_max_wass_dist = np.zeros((res_per_test, train_vals.size, num_tests, noise_vals.size, alpha_values.size-1))
+    if save_eigenvals:
+        eigenvals_in = np.zeros((res_per_test, train_vals.size, noise_vals.size, reg_train_times.size), dtype = object)
 
     print(np.arange(res_per_test, dtype = int))
     print(np.arange(num_trains, dtype = int))
@@ -127,11 +129,19 @@ def main(argv):
             valid_time[i,j,m,k,:,:,l]    = np.transpose(np.loadtxt(os.path.join(raw_data_folder, 'valid_time_res%d_train%d_test%d_noise%e_regtrain%d.csv' % (i,j,m,noise,reg_train_time)), delimiter = ','))
         if pmap:
             pmap_max_wass_dist[i,j,:,k,:,l] = np.transpose(np.loadtxt(os.path.join(raw_data_folder, 'pmap_max_wass_dist_res%d_train%d_noise%e_regtrain%d.csv' % (i,j,noise,reg_train_time)), delimiter = ','))
+        if save_eigenvals:
+            eigenvals_in[i,j,k,l] = np.loadtxt(os.path.join(raw_data_folder, 'gradreg_eigenvals_res%d_train%d_noise%e_regtrain%d.csv' % (i,j,noise,reg_train_time)), delimiter = ',')
         if save_time_rms:
             for m in list(range(num_tests)):
                 #mean_all[i,j,l,k] = np.loadtxt(os.path.join(raw_data_folder, 'mean_all_res%d_train%d_test%d_noise%e.csv' % (i,j,l,noise)), delimiter = ',')
                 #variances_all[i,j,l,k] = np.loadtxt(os.path.join(raw_data_folder, 'variances_all_res%d_train%d_test%d_noise%e.csv' % (i,j,l,noise)), delimiter = ',')
                 rms[i,j,m,k,:,:,l] =np.transpose(np.loadtxt(os.path.join(raw_data_folder, 'rms_res%d_train%d_test%d_noise%e_regtrain%d.csv' % (i,j,m,noise,reg_train_time)), delimiter = ','))
+    if save_eigenvals:
+        eigenvals = np.zeros((res_per_test, train_vals.size, noise_vals.size, reg_train_times.size, eigenvals_in[0,0,0,0].size))
+        for i,j,k,l in product(np.arange(res_per_test,dtype=int), np.arange(train_vals.size,dtype = int), np.arange(noise_vals.size,dtype=int), np.arange(reg_train_times.size,dtype = int)):
+            eigenvals[i,j,k,l] = eigenvals_in[i,j,k,l]
+        print('Eigenvals shape:')
+        print(eigenvals.shape)
     load_toc = time.perf_counter()
     print('All data loaded in %0.2f sec.' % (load_toc - load_tic))
 
@@ -181,6 +191,15 @@ def main(argv):
 
         if pmap:
             data_out['pmap_max_wass_dist'] = pmap_max_wass_dist[all_res, all_train_idx, all_test, all_noise_idx, all_reg_idx, all_reg_train_idx]
+        if save_eigenvals:
+            print(data_out[all_test == 0].shape)
+            print(data_out[all_test == 0][['res','train','test','noise','reg','reg_train']])
+            print(eigenvals[all_res, all_train_idx, all_noise_idx, all_reg_train_idx].shape)
+            print(['eig%d' % (i+1) for i in range(eigenvals.shape[-1])])
+            eigenval_idx = (all_test == 0) & (all_reg_idx == 0)
+            data_out.at[eigenval_idx, ['eig%d' % (i+1) for i in range(eigenvals.shape[-1])]] = \
+                eigenvals[all_res[eigenval_idx],all_train_idx[eigenval_idx],all_noise_idx[eigenval_idx],\
+                all_reg_train_idx[eigenval_idx]]
         if save_time_rms:
             #data_out = pd.concat([data_out, pd.DataFrame(mean_all[all_res, all_train_idx, all_test, all_noise_idx, :, all_reg_idx],\
             #        columns = ['mean_all%d' % i for i in range((rkTime-split)+1)])], axis = 1)
@@ -214,8 +233,8 @@ def main(argv):
         save_toc = time.perf_counter()
         print('Time to compress and save data: %0.2f sec.' % ((save_toc-save_tic)))
 
-    elif return_all and savepred:
-        raise ValueError
+    #elif return_all and savepred:
+    #    raise ValueError
     else:
         best_stable_frac = np.zeros((res_per_test, train_vals.size, noise_vals.size, reg_train_times.size))
         best_train_mean_rms = np.zeros((res_per_test, train_vals.size, noise_vals.size, reg_train_times.size))

helpers/get_run_opts.py
@@ -37,6 +37,7 @@ def get_run_opts(argv, runflag = True):
         num_cpus = 20
         metric = 'mss_var'
         return_all = False
+        save_eigenvals = False
         max_valid_time = 500
         noise_streams_per_test = 5
         noise_values_array = np.logspace(-3, 0, num = 19, base = 10)[5:11]
@@ -54,7 +55,7 @@ def get_run_opts(argv, runflag = True):
                     'tests=', 'trains=', 'savepred=', 'tau=', 'rho=',
                     'sigma=', 'leakage=', 'bias_type=', 'debug=', 'win_type=',
                     'machine=', 'num_cpus=', 'pmap=', 'parallel=', 'metric=','returnall=',
-                    'savetime=', 'noisevals=', 'regvals=', 'maxvt=', 'noisestreams=',
+                    'savetime=', 'saveeigenvals=','noisevals=', 'regvals=', 'maxvt=', 'noisestreams=',
                     'squarenodes=', 'resonly=', 'importres=','importtrain=',
                     'importtest=','importnoise=','regtraintimes=','discardlen=',
                     'prior='])
@@ -71,6 +72,14 @@ def get_run_opts(argv, runflag = True):
             elif opt == '-r':
                 noise_realizations = int(arg)
                 print('Noise Realizations: %d' % noise_realizations)
+            elif opt == '--saveeigenvals':
+                if arg == 'True':
+                    save_eigenvals = True
+                elif arg == 'False':
+                    save_eigenvals = False
+                else:
+                    raise ValueError
+                print('Save grad reg eigenvalues: %s' % arg)
             elif opt == '--prior':
                 prior = str(arg)
                 print('Prior type: %s' % prior)
@@ -251,15 +260,15 @@ def get_run_opts(argv, runflag = True):
             elif system in ['KS', 'KS_d2175']:
                 tau = 0.25
     else:
-        train_time, res_size, noise_realizations, save_time_rms, metric,\
+        train_time, res_size, noise_realizations, save_time_rms, save_eigenvals, metric,\
                 return_all, machine, rho, sigma, leakage, tau, win_type, \
                 bias_type, res_per_test, num_tests, num_trains, savepred, \
                 noisetype, traintype, system, squarenodes, resonly, prior, import_res,\
                 import_train, import_test, import_noise, reg_train_times,\
                 discard_time = argv
-    if return_all and savepred:
-        print('Cannot return results for all parameters and full predictions due to memory constraints.')
-        raise ValueError
+    #if return_all and savepred:
+    #    print('Cannot return results for all parameters and full predictions due to memory constraints.')
+    #    raise ValueError
     if isinstance(reg_train_times, np.ndarray) or isinstance(reg_train_times, list):
         if (reg_train_times[0] != train_time - discard_time or len(reg_train_times) != 1) and (traintype in ['normal','normalres1','normalres2','rmean','rmeanres1',\
                 'rmeanres2','rplusq','rplusqres1','rplusqres2'] or 'confined' in traintype):
@@ -272,19 +281,19 @@ def get_run_opts(argv, runflag = True):
         print('Prior type not recognized.')
         raise ValueError
     if import_res:
-        iresflag = 'ires_'
+        iresflag = '_ires'
     else:
         iresflag = ''
     if import_train:
-        itrainflag = 'itrain_'
+        itrainflag = '_itrain'
     else:
         itrainflag = ''
     if import_test:
-        itestflag = 'itest_'
+        itestflag = '_itest'
     else:
         itestflag = ''
     if import_noise:
-        inoiseflag = 'inoise_'
+        inoiseflag = '_inoise'
     else:
         inoiseflag = ''
     if prior == 'zero':
@@ -292,21 +301,25 @@ def get_run_opts(argv, runflag = True):
     else:
         prior_str = '_prior_%s' % prior
     if savepred:
-        predflag = 'wpred_'
+        predflag = '_wpred'
     else:
         predflag = ''
     if save_time_rms:
-        timeflag = 'savetime_'
+        timeflag = '_savetime'
     else:
         timeflag = ''
     if squarenodes:
-        squarenodes_flag = 'squarenodes_'
+        squarenodes_flag = '_squarenodes'
     else:
         squarenodes_flag = ''
     if resonly:
-        resonly_flag = 'resonly_'
+        resonly_flag = '_resonly'
     else:
         resonly_flag = ''
+    if save_eigenvals:
+        eigenval_flag = '_wgradeigs'
+    else:
+        eigenval_flag = ''
     if machine == 'skynet':
         root_folder = '/h/awikner/res-noise-stabilization/'
     elif machine == 'deepthought2':
@@ -317,19 +330,19 @@ def get_run_opts(argv, runflag = True):
 
     if not return_all:
         data_folder = 'Data/%s_noisetest_noisetype_%s_traintype_%s/' % (system, noisetype, traintype)
-        run_name = '%s_%s%s%s%s%s%s%s%srho%0.1f_sigma%1.1e_leakage%0.3f_win_%s_bias_%s_tau%0.2f_%dnodes_%dtrain_%dreals_noisetype_%s_traintype_%s%s_metric_%s' \
-             % (system,resonly_flag,predflag, timeflag, squarenodes_flag, iresflag, itrainflag, itestflag, inoiseflag, rho, sigma, leakage, win_type, bias_type, tau, res_size, \
+        run_name = '%s%s%s%s%s%s%s%s%s%s_rho%0.1f_sigma%1.1e_leakage%0.3f_win_%s_bias_%s_tau%0.2f_%dnodes_%dtrain_%dreals_noisetype_%s_traintype_%s%s_metric_%s' \
+             % (system,resonly_flag,predflag, timeflag, eigenval_flag, squarenodes_flag, iresflag, itrainflag, itestflag, inoiseflag, rho, sigma, leakage, win_type, bias_type, tau, res_size, \
              train_time, noise_realizations, noisetype, traintype, prior_str, metric)
     elif return_all:
         data_folder = 'Data/%s_noisetest_noisetype_%s_traintype_%s/' % (system, noisetype, traintype)
-        run_name = '%s_%s%s%s%s%s%s%s%srho%0.1f_sigma%1.1e_leakage%0.3f_win_%s_bias_%s_tau%0.2f_%dnodes_%dtrain_%dreals_noisetype_%s_traintype_%s%s' % (system,resonly_flag,predflag, timeflag, squarenodes_flag, iresflag, itrainflag, itestflag, inoiseflag, rho, sigma, leakage, win_type, bias_type, tau,res_size, train_time, noise_realizations, noisetype, traintype, prior_str)
+        run_name = '%s%s%s%s%s%s%s%s%s%s_rho%0.1f_sigma%1.1e_leakage%0.3f_win_%s_bias_%s_tau%0.2f_%dnodes_%dtrain_%dreals_noisetype_%s_traintype_%s%s' % (system,resonly_flag,predflag, timeflag, eigenval_flag, squarenodes_flag, iresflag, itrainflag, itestflag, inoiseflag, rho, sigma, leakage, win_type, bias_type, tau,res_size, train_time, noise_realizations, noisetype, traintype, prior_str)
     if runflag:
         if not os.path.isdir(os.path.join(root_folder, data_folder)):
             os.mkdir(os.path.join(root_folder, data_folder))
         if not os.path.isdir(os.path.join(os.path.join(root_folder, data_folder), run_name + '_folder')):
             os.mkdir(os.path.join(os.path.join(root_folder, data_folder), run_name + '_folder'))
 
-        return root_folder, data_folder, run_name, system, noisetype, traintype, savepred, save_time_rms, squarenodes, rho,\
+        return root_folder, data_folder, run_name, system, noisetype, traintype, savepred, save_time_rms, save_eigenvals, squarenodes, rho,\
             sigma, leakage, win_type, bias_type, tau, res_size, train_time, noise_realizations, noise_streams_per_test,\
             noise_values_array,alpha_values, res_per_test, num_trains, num_tests, debug_mode, pmap, metric, \
             return_all, ifray, machine, max_valid_time, prior, import_res, import_train, import_test, import_noise, \